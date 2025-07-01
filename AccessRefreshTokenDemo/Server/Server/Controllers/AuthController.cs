aab1bc2f14cac38cd8e723279322daf77d76f59a
Mylo Faulkner <mylofaulkner27@gmail.com>
2025-07-01 23:58:54 +0100

    login

--- AccessRefreshTokenDemo/Server/Server/Controllers/AuthController.cs
+++ AccessRefreshTokenDemo/Server/Server/Controllers/AuthController.cs
@@ -1,27 +1,41 @@
 using Microsoft.AspNetCore.Mvc;
 using Server.Models;
 using Server.Services.Interfaces;
+using YamlDotNet.Core.Tokens;
 
 namespace Server.Controllers;
 
 [ApiController]
 [Route("api/auth")]
-public class AuthController(IPasswordService passwordService) : ControllerBase
+public class AuthController(IAuthService authService, IConfiguration configuration) : ControllerBase
 {
-    private readonly IPasswordService _passwordService = passwordService;
+    private static User? _user;
 
     [HttpPost("register")]
     public ActionResult<User> Register(UserDto req)
     {
-        byte[] salt = _passwordService.GenerateSalt(16);
-        byte[] hashedPassword = _passwordService.HashPassword(req.PlainPassword, salt);
+        byte[] salt = authService.GenerateSalt(16);
+        byte[] hashedPassword = authService.HashPassword(req.PlainPassword, salt);
         User user = new()
         {
             Username = req.Username,
             HashedPassword = hashedPassword,
             Salt = salt,
         };
-
+        _user = user;
         return Ok(user);
+    }
+
+    [HttpPost("login")]
+    public ActionResult<string> Login(UserDto req)
+    {
+        if (
+            _user.Username != req.Username
+            || !authService.VerifyPassword(req.PlainPassword, _user.HashedPassword, _user.Salt)
+        )
+            return BadRequest("Invalid username or password");
+
+        string token = authService.GenerateToken(_user);
+        return Ok(token);
     }
 }
