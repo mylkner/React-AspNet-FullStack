38f52b59acaf853560bc313afaf9ba28859305ad
Mylo Faulkner <mylofaulkner27@gmail.com>
2025-07-01 23:59:41 +0100

    generate access token method

--- AccessRefreshTokenDemo/Server/Server/Services/AuthService.cs
+++ AccessRefreshTokenDemo/Server/Server/Services/AuthService.cs
@@ -1,13 +1,18 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Security.Cryptography;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using Server.Models;
 using Server.Services.Interfaces;
 
 namespace Server.Services;
 
-public class AuthService : IAuthService
+public class AuthService(IConfiguration configuration) : IAuthService
 {
     public byte[] HashPassword(string password, byte[] salt)
     {
-        using Rfc2898DeriveBytes pbkdf2 = new(password, salt, 100000, HashAlgorithmName.SHA256);
+        using Rfc2898DeriveBytes pbkdf2 = new(password, salt, 100000, HashAlgorithmName.SHA512);
         return pbkdf2.GetBytes(32);
     }
 
@@ -25,5 +30,27 @@ public class AuthService : IAuthService
         using var rng = RandomNumberGenerator.Create();
         rng.GetBytes(salt);
         return salt;
+    }
+
+    public string GenerateToken(User user)
+    {
+        List<Claim> claims = [new Claim(ClaimTypes.Name, user.Username)];
+
+        SigningCredentials creds = new(
+            new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(configuration.GetValue<string>("AppSettings:Key")!)
+            ),
+            SecurityAlgorithms.HmacSha512
+        );
+
+        JwtSecurityToken token = new(
+            issuer: configuration.GetValue<string>("AppSettings:Issuer"),
+            audience: configuration.GetValue<string>("AppSettings:Audience"),
+            claims: claims,
+            expires: DateTime.UtcNow.AddMinutes(30),
+            signingCredentials: creds
+        );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
     }
 }
--- AccessRefreshTokenDemo/Server/Server/Services/Interfaces/IAuthService.cs
+++ AccessRefreshTokenDemo/Server/Server/Services/Interfaces/IAuthService.cs
@@ -1,3 +1,5 @@
+using Server.Models;
+
 namespace Server.Services.Interfaces;
 
 public interface IAuthService
@@ -5,4 +7,5 @@ public interface IAuthService
     public byte[] HashPassword(string password, byte[] salt);
     public bool VerifyPassword(string inputtedPassword, byte[] hashedPassword, byte[] salt);
     public byte[] GenerateSalt(int size);
+    public string GenerateToken(User user);
 }
