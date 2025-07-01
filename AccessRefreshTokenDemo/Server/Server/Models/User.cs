8bcbe2e964e45bbea87e87236fa41edd0b139006
Mylo Faulkner <mylofaulkner27@gmail.com>
2025-07-01 22:37:45 +0100

    added salt

--- AccessRefreshTokenDemo/Server/Server/Models/User.cs
+++ AccessRefreshTokenDemo/Server/Server/Models/User.cs
@@ -3,5 +3,6 @@ namespace Server.Models;
 public class User
 {
     public string Username { get; set; } = string.Empty;
-    public string HashedPassword { get; set; } = string.Empty;
+    public byte[] HashedPassword { get; set; } = [];
+    public byte[] Salt { get; set; } = [];
 }
