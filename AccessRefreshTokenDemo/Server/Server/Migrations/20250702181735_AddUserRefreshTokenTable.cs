69100fd3405e6e2bc547c6c5b3a2a5659a015229
Mylo Faulkner <mylofaulkner27@gmail.com>
2025-07-02 19:18:00 +0100

    add table for userrefreshtokens

---
+++ AccessRefreshTokenDemo/Server/Server/Migrations/20250702181735_AddUserRefreshTokenTable.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserRefreshTokenTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserRefreshToken_Users_UserId",
+                table: "UserRefreshToken"
+            );
+
+            migrationBuilder.DropPrimaryKey(name: "PK_UserRefreshToken", table: "UserRefreshToken");
+
+            migrationBuilder.RenameTable(name: "UserRefreshToken", newName: "UserRefreshTokens");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_UserRefreshToken_UserId",
+                table: "UserRefreshTokens",
+                newName: "IX_UserRefreshTokens_UserId"
+            );
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_UserRefreshTokens",
+                table: "UserRefreshTokens",
+                column: "Id"
+            );
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserRefreshTokens_Users_UserId",
+                table: "UserRefreshTokens",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserRefreshTokens_Users_UserId",
+                table: "UserRefreshTokens"
+            );
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_UserRefreshTokens",
+                table: "UserRefreshTokens"
+            );
+
+            migrationBuilder.RenameTable(name: "UserRefreshTokens", newName: "UserRefreshToken");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_UserRefreshTokens_UserId",
+                table: "UserRefreshToken",
+                newName: "IX_UserRefreshToken_UserId"
+            );
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_UserRefreshToken",
+                table: "UserRefreshToken",
+                column: "Id"
+            );
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserRefreshToken_Users_UserId",
+                table: "UserRefreshToken",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade
+            );
+        }
+    }
+}
