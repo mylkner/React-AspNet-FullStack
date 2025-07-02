0b897709f9f90d68ef923d3d0d5058b34b5db1bb
Mylo Faulkner <mylofaulkner27@gmail.com>
2025-07-02 19:48:37 +0100

    remove using statement

--- AccessRefreshTokenDemo/Server/Server/Services/AuthService.cs
+++ AccessRefreshTokenDemo/Server/Server/Services/AuthService.cs
@@ -2,7 +2,6 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Security.Cryptography;
 using System.Text;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Server.Data;
