dc7d993d02a2c401e610ddd439dabc83cc564c2d
Mylo Faulkner <mylofaulkner27@gmail.com>
2025-07-02 15:32:10 +0100

    changed id from int to guid

--- AccessRefreshTokenDemo/Server/Server/Models/User.cs
+++ AccessRefreshTokenDemo/Server/Server/Models/User.cs
@@ -2,7 +2,7 @@ namespace Server.Models;
 
 public class User
 {
-    public int Id { get; set; }
+    public Guid Id { get; set; }
     public string Username { get; set; } = string.Empty;
     public byte[] HashedPassword { get; set; } = [];
     public byte[] Salt { get; set; } = [];
