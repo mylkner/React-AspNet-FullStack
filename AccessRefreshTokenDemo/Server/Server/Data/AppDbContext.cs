211409f6039b253380e0cf33f5706cbf2fb88a1d
Mylo Faulkner <mylofaulkner27@gmail.com>
2025-07-02 15:31:57 +0100

    Fixed type in DbContextOptions

--- AccessRefreshTokenDemo/Server/Server/Data/AppDbContext.cs
+++ AccessRefreshTokenDemo/Server/Server/Data/AppDbContext.cs
@@ -3,7 +3,7 @@ using Server.Models;
 
 namespace Server.Data;
 
-public class AppDbContext(DbContextOptions<DbContext> options) : DbContext(options)
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
     public DbSet<User> Users { get; set; } = null!;
 }
