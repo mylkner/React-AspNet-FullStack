597a6a48967df4378bbfc989e504cc64ba48ca8b
Mylo Faulkner <mylofaulkner27@gmail.com>
2025-07-02 16:59:02 +0100

    add user role and migrate db

--- AccessRefreshTokenDemo/Server/Server/Models/User.cs
+++ AccessRefreshTokenDemo/Server/Server/Models/User.cs
@@ -6,4 +6,5 @@ public class User
     public string Username { get; set; } = string.Empty;
     public byte[] HashedPassword { get; set; } = [];
     public byte[] Salt { get; set; } = [];
+    public string Role { get; set; } = string.Empty;
 }
