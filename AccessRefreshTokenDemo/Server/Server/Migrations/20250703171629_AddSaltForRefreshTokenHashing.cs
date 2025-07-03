21ed6323264731d221f6a061311647f24054b72b
Mylo Faulkner <mylofaulkner27@gmail.com>
2025-07-03 18:16:47 +0100

    add slat for refreshtoken hashing

---
+++ AccessRefreshTokenDemo/Server/Server/Migrations/20250703171629_AddSaltForRefreshTokenHashing.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSaltForRefreshTokenHashing : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Salt",
+                table: "UserRefreshTokens",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Salt",
+                table: "UserRefreshTokens");
+        }
+    }
+}
--- AccessRefreshTokenDemo/Server/Server/Models/Db/UserRefreshToken.cs
+++ AccessRefreshTokenDemo/Server/Server/Models/Db/UserRefreshToken.cs
@@ -4,6 +4,7 @@ public class UserRefreshToken
 {
     public Guid Id { get; set; }
     public string RefreshToken { get; set; } = string.Empty;
+    public string Salt { get; set; } = string.Empty;
     public DateTime Expiry { get; set; }
 
     public User User { get; set; } = null!;
