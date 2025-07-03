72d0c737b839344eeefac142f0205a943d1d2b6e
Mylo Faulkner <mylofaulkner27@gmail.com>
2025-07-03 18:13:38 +0100

    can get user id from tokenvalue and deviceid is gone so no need for class

--- AccessRefreshTokenDemo/Server/Server/Models/Dtos/RefreshTokenDto.cs
+++
@@ -1,8 +0,0 @@
-namespace Server.Models.Dtos;
-
-public class RefreshTokenDto
-{
-    public string UserId { get; set; } = string.Empty;
-    public string DeviceId { get; set; } = string.Empty;
-    public string TokenValue { get; set; } = string.Empty;
-}
