3a55a02e270c5fd7bf4ec56b7de7174a38650800
Mylo Faulkner <mylofaulkner27@gmail.com>
2025-07-03 13:58:25 +0100

    moved folder

---
+++ AccessRefreshTokenDemo/Server/Server/Models/Db/User.cs
@@ -0,0 +1,12 @@
+namespace Server.Models.Db;
+
+public class User
+{
+    public Guid Id { get; set; }
+    public string Username { get; set; } = string.Empty;
+    public string HashedPassword { get; set; } = string.Empty;
+    public string Salt { get; set; } = string.Empty;
+    public string Role { get; set; } = string.Empty;
+
+    public List<UserRefreshToken> RefreshTokens { get; set; } = [];
+}
