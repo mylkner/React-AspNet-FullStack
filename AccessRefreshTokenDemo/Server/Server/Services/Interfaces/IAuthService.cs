a7eb422b581b35becfe8cacd423d06faf2165790
Mylo Faulkner <mylofaulkner27@gmail.com>
2025-07-03 13:58:45 +0100

    updated args

--- AccessRefreshTokenDemo/Server/Server/Services/Interfaces/IAuthService.cs
+++ AccessRefreshTokenDemo/Server/Server/Services/Interfaces/IAuthService.cs
@@ -1,16 +1,13 @@
-using Server.Models;
-using Server.Models.Dtos.AuthDtos;
+using Server.Models.Db;
+using Server.Models.Dtos;
 
 namespace Server.Services.Interfaces;
 
 public interface IAuthService
 {
-    public Task<User?> RegisterAsync(RegisterDto req);
-    public Task<string?> LoginAsync(LoginDto req, HttpContext context);
-    public Task<User?> LogoutAsync(UserDeviceIdsDto req, HttpContext context);
-    public Task<User?> DeleteAsync(DeleteDto req, HttpContext context);
-    public Task<string?> ValidateAndReplaceRefreshTokenAsync(
-        UserDeviceIdsDto req,
-        HttpContext context
-    );
+    public Task<User?> RegisterAsync(UserDto req);
+    public Task<string?> LoginAsync(UserDto req, HttpContext context);
+    public Task<User?> LogoutAsync(HttpContext context);
+    public Task<User?> DeleteAsync(HttpContext context);
+    public Task<string?> ValidateAndReplaceRefreshTokenAsync(HttpContext context);
 }
